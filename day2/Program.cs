ab67582ed801e9742646a1306d65adfd7b5e5a6b
nafees <nafees.hassan@ili.digital>
2025-08-06 12:20:06 +0500

    explanations for Program.cs

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,25 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Registers controllers with http server.
+// If not done: UseAuthorization crashes, MapControllers crashes.
+// And if those are commented out too: Then Server runs, with no exposed endpoints
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
+
+// Adds the service that reads my controllers' routes, and generates OpenAPI documentation in JSON format
+// which swagger UI uses
+// also, this is removed in dotnet 9.0
 builder.Services.AddSwaggerGen();
 
+// finally build as WebApplication from this WebApplicationBuilder
+// this is where thhose appsettings.json etc get read
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// only include swagger UI in dev builds (not in release, final build)
 if (app.Environment.IsDevelopment())
 {
+    // this is what generates the OpenAPI json file, and serves it
     app.UseSwagger();
+    // and this serves the swagger frontend
     app.UseSwaggerUI();
 }
 
+// redirects requests coming on http:// to https:// using the PermantlyMoved response code
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// authorization middleware. we not using it right now, so does not matter
+//app.UseAuthorization();
 
+// Maps controllers to the routes defined using [Route] and [HttpGet/Post/Etc]
+// If not done, swagger sees the endpoints, but they are not exposed. All return 404
 app.MapControllers();
 
+// run the server, listen for requests, call middleware and controllers' routes etc
 app.Run();
